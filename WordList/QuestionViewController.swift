9ccd82c410da54429d165a909fa560d94c0bed98
YudaiTakahashi <yudai02012002@icloud.com>
2021-05-13 09:47:56 +0900

    単語帳アプリの完成

--- WordList/QuestionViewController.swift
+++ WordList/QuestionViewController.swift
@@ -15,7 +15,7 @@ class QuestionViewController: UIViewController {
     
     var isAnswered: Bool = false   //回答したか・次の質問へ行くかの判断
     var wordArray: [Dictionary<String,String>] = [] //UserDefaultsからとる配列
-    var nowNumber: Int = 0
+    var nowNumber: Int = 0  //現在の回答数
     let saveDate = UserDefaults.standard
 
     override func viewDidLoad() {
@@ -31,4 +31,35 @@ class QuestionViewController: UIViewController {
         wordArray.shuffle()
         questionLabel.text = wordArray[nowNumber]["english"]
     }
+    
+    @IBAction func nextButtonTapped() {
+        //回答したか
+        if isAnswered {
+            //次の問題へ
+            nowNumber += 1
+            answerLabel.text = ""
+            
+            //次の問題を表示するか
+            if nowNumber < wordArray.count{
+                //次の問題を表示
+                questionLabel.text = wordArray[nowNumber]["english"]
+                //isAnsweredをfalseにする
+                isAnswered = false
+                //ボタンのタイトルを変更
+                nextButton.setTitle("答えを表示", for: .normal)
+            } else {
+                nowNumber = 0
+                performSegue(withIdentifier: "toFinishView", sender: nil)
+            }
+        } else {
+            //答えを表示
+            answerLabel.text = wordArray[nowNumber]["japanese"]
+            //isAnsweredをTrueに
+            isAnswered = true
+            //ボタンのタイトルを変更
+            nextButton.setTitle("次へ", for: .normal)
+        }
+    }
 }
+
+
